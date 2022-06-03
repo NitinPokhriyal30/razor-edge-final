2e5a0fb446915fa831129cf076b938b245c97029
NitinPokhriyal30 <nitinpokhriyal30@gmail.com>
2022-06-03 13:57:23 +0530

    First commit

---
+++ client/src/Admin/adminComponents/Home/HomeProductList.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
+import { Link } from "react-router-dom";
+
+const HomeProductList = () => {
+  const [productList, setProductList] = useState([]);
+
+  useEffect(() => {
+    Axios.get("https://razor-edge-app.herokuapp.com/homeproductlist").then((res) =>
+      setProductList(res.data)
+    );
+  }, []);
+
+  return (
+    <>
+      <div className="row">
+        {productList.map((value, index) => (
+          <div className="col-xl-2 col-lg-2 col-md-4 col-sm-4" key={index}>
+            <div className="product-page-icon-container">
+              <img
+                src={`https://razor-edge-app.herokuapp.com/images/${value.img}`}
+                className="multi"
+                alt="multi"
+              />
+              <p>{value.title}</p>
+              <Link
+                to={`/admin-dashboard/productList/${value._id}`}
+                className="product-edit-btn "
+              >
+                Edit
+                <i className="fa-solid fa-pen-to-square"></i>
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default HomeProductList;
---
+++ client/src/components/Career/CareerGallery.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import Axios from "axios";
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+
+const CareerGallery = () => {
+
+    const[careerGallery, setCareerGallery] = useState([]);
+
+  useEffect(() => {
+    Axios.get("https://razor-edge-app.herokuapp.com/career-gallery").then((res) =>
+    setCareerGallery(res.data)
+    );
+  }, []);
+
+  return (
+    <ImageList 
+    // sx={{ width: 1310, height: 450 }} 
+    cols={3}>
+      {careerGallery.map((item) => (
+        <ImageListItem key={item.img}>
+          <img
+            src={`https://razor-edge-app.herokuapp.com/images/${item.img}?w=164&h=164&fit=crop&auto=format`}
+            srcSet={`https://razor-edge-app.herokuapp.com/images/${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}  
+            alt={item.img}
+            loading="lazy"
+          />
+        </ImageListItem>
+      ))}
+    </ImageList>
+  )
+}
+
+export default CareerGallery
